17f7578d499c81bf789aea35ffe2582a5b06aada
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2024-01-07 11:25:47 +0700

    Merge conflict 3

--- src/main/java/vn/hdweb/team9/controller/admin/CategoryController.java
+++ src/main/java/vn/hdweb/team9/controller/admin/CategoryController.java
@@ -1,8 +1,12 @@
 package vn.hdweb.team9.controller.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/admin/category")
@@ -16,5 +20,19 @@ public class CategoryController {
     @GetMapping("/new")
     public String createCategory() {
         return "admin/category/createCategory";
+    }
+    
+    @GetMapping("/upload")
+    public String getUpload() {
+        return "uploadFile";
+    }
+    
+    @PostMapping(value = "/upload")
+    public String upLoad(Model model, @RequestParam("image") MultipartFile file) {
+        String filePath = UploadController.uploadImage(model, file);
+        if (filePath != null) {
+            System.out.println(filePath);
+        }
+        return "uploadFile";
     }
 }
--- src/main/java/vn/hdweb/team9/domain/entity/Category.java
+++ src/main/java/vn/hdweb/team9/domain/entity/Category.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @Entity
 @Getter @Setter
-@Table(name = "category")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
